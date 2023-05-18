c8636e3659494e6e48bdeadafd6ef4fa637009bb
Mario Rocha <eng.mec.mario.rocha@gmail.com>
2023-05-18 17:59:23 -0300

    Comment Service Classes

--- src/main/kotlin/me/dio/credit/application/system/service/impl/CreditService.kt
+++ src/main/kotlin/me/dio/credit/application/system/service/impl/CreditService.kt
@@ -14,32 +14,33 @@ class CreditService(
   private val creditRepository: CreditRepository,
   private val customerService: CustomerService
 ) : ICreditService {
+
+  // Método responsável por salvar uma instância de Credit
   override fun save(credit: Credit): Credit {
-    this.validDayFirstInstallment(credit.dayFirstInstallment)
+    this.validDayFirstInstallment(credit.dayFirstInstallment) // Valida a data do primeiro vencimento
     credit.apply {
-      customer = customerService.findById(credit.customer?.id!!)
+      customer = customerService.findById(credit.customer?.id!!) // Obtém o cliente associado ao crédito
     }
-    return this.creditRepository.save(credit)
+    return this.creditRepository.save(credit) // Salva a instância de Credit no repositório
   }
 
+  // Método responsável por encontrar todos os créditos associados a um cliente
   override fun findAllByCustomer(customerId: Long): List<Credit> =
-    this.creditRepository.findAllByCustomerId(customerId)
+    this.creditRepository.findAllByCustomerId(customerId) // Retorna os créditos encontrados no repositório
 
+  // Método responsável por encontrar um crédito pelo código e verificar se pertence ao cliente especificado
   override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
     val credit: Credit = (this.creditRepository.findByCreditCode(creditCode)
-      ?: throw BusinessException("Creditcode $creditCode not found"))
-    return if (credit.customer?.id == customerId) credit
-    else throw IllegalArgumentException("Contact admin")
-    /*if (credit.customer?.id == customerId) {
-      return credit
-    } else {
-      throw RuntimeException("Contact admin")
-    }*/
+      ?: throw BusinessException("Creditcode $creditCode not found")) // Busca o crédito pelo código no repositório, ou lança exceção se não encontrado
+
+    return if (credit.customer?.id == customerId) credit // Verifica se o cliente associado ao crédito é o mesmo especificado. Se for, retorna o crédito.
+    else throw IllegalArgumentException("Contact admin") // Caso contrário, lança uma exceção de argumento inválido.
   }
 
+  // Método privado que valida a data do primeiro vencimento
   private fun validDayFirstInstallment(dayFirstInstallment: LocalDate): Boolean {
-    return if (dayFirstInstallment.isBefore(LocalDate.now().plusMonths(3))) true
-    else throw BusinessException("Invalid Date")
+    return if (dayFirstInstallment.isBefore(LocalDate.now().plusMonths(3))) true // Verifica se a data do primeiro vencimento é anterior a três meses a partir da data atual.
+    else throw BusinessException("Invalid Date") // Caso contrário, lança uma exceção de negócio indicando uma data inválida.
   }
 }
 
--- src/main/kotlin/me/dio/credit/application/system/service/impl/CustomerService.kt
+++ src/main/kotlin/me/dio/credit/application/system/service/impl/CustomerService.kt
@@ -10,11 +10,14 @@ import org.springframework.stereotype.Service
 class CustomerService(
   private val customerRepository: CustomerRepository
 ): ICustomerService {
+  // Método responsável por salvar uma instância de Customer
   override fun save(customer: Customer): Customer = this.customerRepository.save(customer)
 
+  // Método responsável por procurar uma instância de Customer
   override fun findById(id: Long): Customer = this.customerRepository.findById(id)
     .orElseThrow{throw BusinessException("Id $id not found") }
 
+  // Método responsável por excluir uma instância de Customer
   override fun delete(id: Long) {
     val customer: Customer = this.findById(id)
     this.customerRepository.delete(customer)
